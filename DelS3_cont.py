5d743eeb6b9c3c025356469b8b84440d8c938f21
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-23 18:04:26 +0100

    Update DelS3_cont.py

--- DelS3_cont.py
+++ DelS3_cont.py
@@ -1,6 +1,12 @@
 # Deleting Content In A S3 Bucket
 
 import boto3
-s3 = boto3.resource("s3")
+
+AWS_REGION = "eu-west-1"
+S3_BUCKET_NAME = "eng122-haider-buck"
+
+s3 = boto3.resource("s3", region_name=AWS_REGION)
 obj = s3.Object("bucket_name", "filename.format")
 obj.delete()
+
+print('S3 object deleted')
