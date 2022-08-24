11c04722050d01a72a7866f26b44108bbfc3cc0d
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-24 10:36:56 +0100

    Update CreateS3_buck.py

--- CreateS3_buck.py
+++ CreateS3_buck.py
@@ -1,6 +1,6 @@
 # Creating S3 in AWS - (Boto3)
 
 import boto3
-s3_client = boto3.client('s3')   #generating S3 client here to form connection
+s3_client = boto3.client('s3')   #generating S3 object here.
 s3_client.create_bucket(Bucket='eng122-haider-botobuck',  # using create bucket bucket command to create bucket
 CreateBucketConfiguration= {'LocationConstraint':'eu-west-1'}) # AWS requries us to be more specific so defining region
